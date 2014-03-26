8d5def2a44f018d4e05760d2e0abf0cfcf61abe8
druid726 <pdoneill26@gmail.com>
2014-03-26 18:16:36 -0400

    fix mobile menu display

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function() { 
+
+	///// MOBILE MENU /////
+
+	$('a.menu-icon').click(function() {
+		$('#menu-links').slideToggle();
+	});
+
+	///// FIX HIDDEN LINKS IN RESIZE /////
+
+	$(window).resize(function() {
+		if ($(window).width() > 700) {
+			$('#menu-links').removeAttr('style');
+		}
+	})
+
+});
+
+
+
+
+
