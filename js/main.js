5bf7a7a073f9def982da45350af1a6d7fbd657c3
druid726 <pdoneill26@gmail.com>
2014-03-30 14:05:40 -0400

    add jquery for navbar media queries

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-$(function() { 
+$(document).ready(function() { 
 
-	///// MOBILE MENU /////
+	/*//// MOBILE MENU ////*/
 
 	$('a.menu-icon').click(function() {
 		$('#menu-links').slideToggle();
 	});
 
-	///// FIX HIDDEN LINKS IN RESIZE /////
+	/*//// FIX HIDDEN LINKS IN RESIZE ////*/
 
 	$(window).resize(function() {
 		if ($(window).width() > 700) {
 			$('#menu-links').removeAttr('style');
 		}
-	})
+	});
 
 });
 
